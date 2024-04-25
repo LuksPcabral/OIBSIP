ffa5e40e510aa484d111597ba452763849ebafa8
Lucas Paulino Cabral <134960214+LuksPcabral@users.noreply.github.com>
2024-04-25 16:52:27 -0300

    Add files via upload

---
+++ Macca`s Menu.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Nutrition Facts from McDonald's Menu`s
+
+
+# In[ ]:
+
+
+# TOP 10 higher in sodium
+
+|   Category  |                          Item                               |  Sodium  |
+|:-----------:|:-----------------------------------------------------------:|:--------:|
+| Chicken&Fish| Chicken McNuggets (40 piece)                                |  3600    |
+| Breakfast   | RBig Breakfast with Hotcakes and Egg Whites (Large Biscuit) |  2290    |
+| Breakfast   | Big Breakfast with Hotcakes (Large Biscuit)                 |  2260    |
+| Breakfast   | Big Breakfast with Hotcakes and Egg Whites (Regular Biscuit)|  2170    |
+| Breakfast   | Big Breakfast with Hotcakes (Regular Biscuit)               |  2150    |
+| Breakfast   | Chicken McNuggets (20 piece)                                |  1800    |
+| Chicken&Fish| Bacon Clubhouse Crispy Chicken Sandwich                     |  1720    |
+| Chicken&Fish| Big Breakfast with Egg Whites (Large Biscuit)               |  1700    |
+| Breakfast   | Big Breakfast (Large Biscuit)                               |  1680    |
+
